0467fed8c9c0467b6aee06fc1b887219958b57ad
valospc1 <valospc1@valos.id>
2026-03-27 22:24:38 +0700

    up

---
+++ copy-script/laptop1-to-pc.sh
@@ -0,0 +1,4 @@
+rsync -avzhp laptop1:/home/myindo/workspace/myindo/hyperledger-demo/organizations/peerOrganizations/org2.example.com/ /home/valos/workspace/myindo/hlf-demo/organizations/peerOrganizations/org2.example.com/
+
+# after running 004-generate-configtx in central pc
+rsync -avzhp /home/valos/workspace/myindo/hlf-demo/config/channel-artifacts/ laptop1:/home/myindo/workspace/myindo/hyperledger-demo/config/channel-artifacts/
